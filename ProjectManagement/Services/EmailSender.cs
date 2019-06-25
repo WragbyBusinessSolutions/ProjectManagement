cceab39d7269a4bcee4a848f07d030354abb8062
Temitayo Adesugba <Tadesugba@wragbysolutions.com>
2019-06-25 15:20:54 +0100

    Worked on the roles authentication on the layoutAside

--- ProjectManagement/Services/EmailSender.cs
+++ ProjectManagement/Services/EmailSender.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace ProjectManagement.MailServices
 {
-   
-    public class EmailSender : IEmailSender
+
+  public class EmailSender : IEmailSender
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
