4d736b5f4551a4b22095184b3e621f24474bc2d5
Tolulope Agabielesin <Tagabielesin@wragbysolutions.com>
2019-06-27 08:54:40 +0100

    ad

--- ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/Controllers/HomeController.cs
@@ -22,6 +22,8 @@ namespace ProjectManagement.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
